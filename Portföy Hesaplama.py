a83cb25a901f35a5f7e451b81134fe4e7bc01d20
Piardian <piardian1111@gmail.com>
2024-06-30 11:42:40 +0300

    Update Portföy Hesaplama.py

--- Portföy Hesaplama.py
+++ Portföy Hesaplama.py
@@ -1,209 +1,252 @@
-import requests
-from tkinter import *
 import tkinter as tk
 from tkinter import ttk
+import yfinance as yf
 
-# Global değişkenler
-yeni_islem = True
+# Küresel değişkenler
 hesap = []
 s1 = []
+yeni_islem = True
+toplam_fiyat = 0
 
-# API anahtarlarınızı buraya ekleyin
-EXCHANGE_RATE_API_KEY = '4eecc3612e92a51ef6045107'
-EXCHANGE_RATE_BASE_URL = f'https://v6.exchangerate-api.com/v6/{EXCHANGE_RATE_API_KEY}/latest'
-
-ALPHA_VANTAGE_API_KEY = 'ONIF6EGUPLH8JJGC'
-ALPHA_VANTAGE_BASE_URL = 'https://www.alphavantage.co/query'
-
-# Döviz kurlarını çekmek için fonksiyon
-def get_exchange_rate(from_currency, to_currency):
-    url = f'{EXCHANGE_RATE_BASE_URL}/{from_currency}'
+def get_stock_price_yahoo(symbol):
     try:
-        response = requests.get(url)
-        response.raise_for_status()  # HTTPError için kontrol
-        data = response.json()
-        if data['result'] == 'success':
-            return data['conversion_rates'][to_currency]
+        stock = yf.Ticker(symbol)
+        hist = stock.history(period="1d", interval="1m")
+        if not hist.empty:
+            latest_price = hist['Close'].iloc[-1]
+            return latest_price
         else:
-            print("API Hatası:", data['error-type'])
+            print("Geçersiz Sembol veya Veri Yok")
             return None
-    except requests.exceptions.RequestException as e:
-        print(f"HTTP Hatası: {e}")
-        return None
-    except ValueError:
-        print("JSON Decode Hatası")
+    except Exception as e:
+        print(f"Hata: {e}")
         return None
 
-# Hisse senedi fiyatlarını çekmek için fonksiyon
-def get_stock_price(symbol):
-    url = f'{ALPHA_VANTAGE_BASE_URL}?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=1min&apikey={ALPHA_VANTAGE_API_KEY}'
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # HTTPError için kontrol
-        data = response.json()
-        time_series = data.get('Time Series (1min)')
-        if time_series:
-            latest_time = sorted(time_series.keys())[0]
-            return time_series[latest_time]['1. open']
-        else:
-            print("API Hatası veya Geçersiz Sembol")
-            return None
-    except requests.exceptions.RequestException as e:
-        print(f"HTTP Hatası: {e}")
-        return None
-    except ValueError:
-        print("JSON Decode Hatası")
-        return None
-
-# Döviz dönüştürme fonksiyonu
-def birim_donustur():
-    from_currency = secim_var1.get()
-    to_currency = secim_var2.get()
-    try:
-        amount = float(giris.get())
-    except ValueError:
-        sonuc_giris.delete(0, 'end')
-        sonuc_giris.insert(0, 'Geçersiz Miktar')
-        return
-
-    rate = get_exchange_rate(from_currency, to_currency)
-    if rate is not None:
-        converted_amount = amount * rate
-        sonuc_giris.delete(0, 'end')
-        sonuc_giris.insert(0, str(converted_amount))
-    else:
-        sonuc_giris.delete(0, 'end')
-        sonuc_giris.insert(0, 'Hata!')
-
-# Kur bilgilerini yenileme fonksiyonu
-
-# Hisse senedi fiyatlarını görüntüleme fonksiyonu
-def hisse_goruntule():
-    symbol = hisse_giris.get()
-    price = get_stock_price(symbol)
-    if price is not None:
-        hisse_sonuc_giris.delete(0, 'end')
-        hisse_sonuc_giris.insert(0, str(price))
-    else:
-        hisse_sonuc_giris.delete(0, 'end')
-        hisse_sonuc_giris.insert(0, 'Hata!')
-
-# Fonksiyonlar
 def yaz(x):
     global yeni_islem
     if yeni_islem:
-        giris.delete(0, 'end')
+        toplam_fiyat_entry.delete(0, 'end')
         yeni_islem = False
-    s = len(giris.get())
-    giris.insert(s, str(x))
+    s = len(toplam_fiyat_entry.get())
+    toplam_fiyat_entry.insert(s, str(x))
+
+def islemler(x):
+    global hesap
+    global s1
+    global yeni_islem
+
+    if yeni_islem and x not in "+-*/%":
+        toplam_fiyat_entry.delete(0, 'end')
+        yeni_islem = False
+
+    if x in "+-*/":
+        try:
+            s1.append(float(toplam_fiyat_entry.get()))
+        except ValueError:
+            toplam_fiyat_entry.delete(0, 'end')
+            toplam_fiyat_entry.insert(0, "Hata")
+            return
+        hesap.append(x)
+        toplam_fiyat_entry.delete(0, 'end')
+    elif x == "%":
+        try:
+            s1.append(float(toplam_fiyat_entry.get()))
+        except ValueError:
+            toplam_fiyat_entry.delete(0, 'end')
+            toplam_fiyat_entry.insert(0, "Hata")
+            return
+        toplam_fiyat_entry.delete(0, 'end')
+    else:
+        toplam_fiyat_entry.insert(END, x)
+
+def hesapla():
+    global s1
+    global hesap
+    global yeni_islem
+
+    try:
+        s1.append(float(toplam_fiyat_entry.get()))
+
+        sonuc = s1[0]
+        for i in range(1, len(s1)):
+            if hesap[i-1] == '+':
+                sonuc += s1[i]
+            elif hesap[i-1] == '-':
+                sonuc -= s1[i]
+            elif hesap[i-1] == '/':
+                if s1[i] != 0:
+                    sonuc /= s1[i]
+                else:
+                    toplam_fiyat_entry.delete(0, 'end')
+                    toplam_fiyat_entry.insert(0, "Hata: Sıfıra bölme")
+                    hesap = []
+                    s1 = []
+                    yeni_islem = True
+                    return
+            elif hesap[i-1] == '*':
+                sonuc *= s1[i]
+
+        sonuc_str = str(sonuc)
+        if sonuc % 1 == 0:
+            sonuc_str = str(int(sonuc))
+
+        toplam_fiyat_entry.delete(0, 'end')
+        toplam_fiyat_entry.insert(0, sonuc_str)
+        hesap = []
+        s1 = []
+        yeni_islem = True
+    except ValueError:
+        toplam_fiyat_entry.delete(0, 'end')
+        toplam_fiyat_entry.insert(0, "Hata")
+        hesap = []
+        s1 = []
+        yeni_islem = True
 
 def sil():
-    giris.delete(len(giris.get()) - 1)
+    toplam_fiyat_entry.delete(len(toplam_fiyat_entry.get()) - 1)
 
 def temizle():
     global hesap
     global s1
     global yeni_islem
-    giris.delete(0, 'end')
-    sonuc_giris.delete(0, 'end')
+    global toplam_fiyat
+
+    toplam_fiyat_entry.delete(0, 'end')
     hesap = []
     s1 = []
     yeni_islem = True
+    toplam_fiyat = 0
+    total_sonuc.delete(0, 'end')
 
-def secim_degisti(event):
-    secilen1 = secim_var1.get()
-    secilen2 = secim_var2.get()
-    print(f"Seçilen 1: {secilen1}")
-    print(f"Seçilen 2: {secilen2}")
+def hisse_goruntule():
+    global toplam_fiyat
+
+    symbol = hisse_giris.get()
+    try:
+        lot_sayisi = int(Lot_giris.get())
+    except ValueError:
+        hisse_sonuc_giris.delete(0, 'end')
+        hisse_sonuc_giris.insert(0, 'Geçersiz Lot Sayısı')
+        return
+    
+    price = get_stock_price_yahoo(symbol)
+    if price is not None:
+        birim = secim_var1.get()
+
+        if lot_sayisi >= 1:
+            total_price = price * lot_sayisi
+            toplam_fiyat += total_price
+
+            toplam_fiyat_entry.delete(0, 'end')
+            toplam_fiyat_entry.insert(0, f"{total_price:.2f} {birim}")
+
+            total_sonuc.delete(0, 'end')
+            total_sonuc.insert(0, f"{toplam_fiyat:.2f} {birim}")
+        
+        hisse_sonuc_giris.delete(0, 'end')
+        hisse_sonuc_giris.insert(0, f"{price:.2f} {birim}")
+    else:
+        hisse_sonuc_giris.delete(0, 'end')
+        hisse_sonuc_giris.insert(0, 'Hata!')
+
+def sadece_rakam_girisi(char):
+    return char.isdigit()
 
 # Pencere ayarları
 window = tk.Tk()
-window.title('Döviz ve Hisse Hesaplama')
-window.geometry("293x240")
+window.title('Porföy Hesaplama')
+window.geometry("293x580")
 window.configure(background='black')
 window.resizable(width=False, height=False)
 
-
-def ikinci_pencere():
-    ikinci_pencere= tk.Frame(window, bg="black" ,bd=2,relief="ridge")
-    ikinci_pencere.place(x=1, y=1,width=80, height=100)
-    Label=tk.Label(ikinci_pencere, text="Pia",fg="red",bg="black",font=("Times", 24))
-    Label.place(x=15, y=35,width=40, height=30)
-    Button(ikinci_pencere, width=1, text="...", fg="black", font=("Helvetica", 9),  background='white',command=ikinci_pencere.destroy).place(height=15,x=1, y=1)
-
-
-# Giriş alanı
-#giris = ttk.Entry(window, width=29, justify=tk.RIGHT, font=("Helvetica", 16))
-#giris.place(height=60, width=265, x=15, y=22)
-
-# Sonuç alanı
-#sonuc_giris = ttk.Entry(window, width=29, justify=tk.RIGHT, font=("Helvetica", 16))
-#sonuc_giris.place(height=60, width=265, x=15, y=90)
-
-# Seçim baloncuğu için bir StringVar oluştur
 secim_var1 = tk.StringVar()
-secim_var2 = tk.StringVar()
 
-# Hisse senedi sembol girişi
-hisse_giris = ttk.Entry(window, width=29, justify=tk.RIGHT, font=("Helvetica", 16))
-hisse_giris.place(height=60, width=265, x=15, y=22)
-tk.Canvas(hisse_giris,Canvas=("hisse kodu giriniz"))
+# Hisse kodu girişi
+hisse_giris_text = tk.StringVar()
+hisse_giris = ttk.Entry(window, width=29, justify=tk.RIGHT, font=("Times",10), textvariable=hisse_giris_text)
+hisse_giris.place(height=30, width=130, x=15, y=22)
+Label = tk.Label(hisse_giris, text="Hisse Kodu", fg="black", bg="white", font=("Helvetica", 9))
+Label.place(height=15, width=70, x=0, y=0)
+
+# Girilen metni büyük harfe çevirme
+hisse_giris_text.trace_add("write", lambda *args: hisse_giris_text.set(hisse_giris_text.get().upper()))
+
+# Lot sayısı girişi
+Lot_giris = ttk.Entry(window, width=29, justify=tk.RIGHT, font=("Times",10), validate="key")
+Lot_giris['validatecommand'] = (Lot_giris.register(sadece_rakam_girisi), '%S')
+Lot_giris.place(height=30, width=130, x=15, y=60)
+Label = tk.Label(Lot_giris, text="Lot sayısı", fg="black", bg="white", font=("Helvetica", 9))
+Label.place(height=15, width=60, x=0, y=0)
+
 # Hisse senedi sonucu
-hisse_sonuc_giris = ttk.Entry(window, width=29, justify=tk.RIGHT, font=("Helvetica", 16))
-hisse_sonuc_giris.place(height=60, width=265, x=15, y=90)
+hisse_sonuc_giris = ttk.Entry(window, width=29, justify=tk.RIGHT, font=("Times",10))
+hisse_sonuc_giris.place(height=30, width=135, x=150, y=22)
+Label = tk.Label(hisse_sonuc_giris, text="Fiyat", fg="black", bg="white", font=("Helvetica", 9))
+Label.place(height=15, width=30, x=0, y=0)
 
-# Hisse senedi butonu
+# Toplam fiyat girişi
+toplam_fiyat_entry = ttk.Entry(window, width=29, justify=tk.RIGHT, font=("Helvetica", 16))
+toplam_fiyat_entry.place(height=50, width=265, x=15, y=170)
+Label = tk.Label(toplam_fiyat_entry, text="Toplam Fiyat", fg="black", bg="white", font=("Helvetica", 9))
+Label.place(height=15, width=80, x=0, y=0)
 
-# Seçenekler listesi
-secenekler = ["USD", "TRY", "EUR", "GBP", "JPY", "CNY", "RUB", "NZD", "CHF", "CAD", "HKD"]
+total_sonuc = ttk.Entry(window, width=29, justify=tk.RIGHT, font=("Helvetica", 16))
+total_sonuc.place(height=50, width=265, x=15, y=100)
+Label = tk.Label(total_sonuc, text="Toplam Sonuç", fg="black", bg="white", font=("Helvetica", 9))
+Label.place(height=15, width=80, x=0, y=0)
 
-# 1. Combobox (Seçim baloncuğu) oluştur
-combobox1 = ttk.Combobox(window, textvariable=secim_var1, values=secenekler, state="readonly")
-combobox1.bind("<<ComboboxSelected>>", secim_degisti)
-combobox1.place(width=60, height=17, x=15, y=133)
+secenekler = ["$", "TRY", "EUR", "GBP", "JPY", "CNY", "RUB", "NZD", "CHF", "CAD", "HKD"]
+combobox1 = ttk.Combobox(toplam_fiyat_entry, textvariable=secim_var1, values=secenekler, state="readonly")
+combobox1.place(width=60, height=17, x=0, y=35)
 
-# 2. Combobox (Seçim baloncuğu) oluştur
-#combobox2 = ttk.Combobox(window, textvariable=secim_var2, values=secenekler, state="readonly")
-#combobox2.bind("<<ComboboxSelected>>", secim_degisti)
-#combobox2.place(width=60, height=17, x=15, y=132)
+ttk.Button(window, text="Hisse Fiyat", command=hisse_goruntule, width=20).place(height=44, x=20, y=230)
 
-
-# Hisse senedi butonu
-ttk.Button(window, text="Hisse Fiyat", command=hisse_goruntule, width=20).place(height=44, x=20, y=170)
-
-window.bind("<Tab>", lambda event: "break")
-#giris.bind("<FocusIn>", lambda event: "break")
-#giris.bind("<Button-1>", lambda event: "break")
-#sonuc_giris.bind("<FocusIn>", lambda event: "break")
-#sonuc_giris.bind("<Button-1>", lambda event: "break")
-combobox1.bind("<FocusIn>", lambda event: "break")
-#combobox2.bind("<FocusIn>", lambda event: "break")
-hisse_sonuc_giris.bind("<FocusIn>", lambda event: "break")
-hisse_sonuc_giris.bind("<Button-1>", lambda event: "break")
-# Silme ve temizleme butonları
 style = ttk.Style()
 style.configure('TButton', font=('Helvetica', 16))
 
-# Sayı butonları
+buttons = [
+    {"text": "1", "command": lambda: yaz(1), "pos": (15, 340)},
+    {"text": "2", "command": lambda: yaz(2), "pos": (85, 340)},
+    {"text": "3", "command": lambda: yaz(3), "pos": (155, 340)},
+    {"text": "4", "command": lambda: yaz(4), "pos": (15, 400)},
+    {"text": "5", "command": lambda: yaz(5), "pos": (85, 400)},
+    {"text": "6", "command": lambda: yaz(6), "pos": (155, 400)},
+    {"text": "7", "command": lambda: yaz(7), "pos": (15, 460)},
+    {"text": "8", "command": lambda: yaz(8), "pos": (85, 460)},
+    {"text": "9", "command": lambda: yaz(9), "pos": (155, 460)},
+    {"text": ".", "command": lambda: yaz("."), "pos": (155, 520)},
+    {"text": "0", "command": lambda: yaz(0), "pos": (15, 520), "width": 10},
+    {"text": "x", "command": lambda: islemler("*"), "pos": (225, 340)},
+    {"text": "÷", "command": lambda: islemler("/"), "pos": (225, 400)},
+    {"text": "+", "command": lambda: islemler("+"), "pos": (225, 460)},
+    {"text": "-", "command": lambda: islemler("-"), "pos": (225, 520)},
+    {"text": "=", "command": hesapla, "pos": (85, 520)},
+    {"text": "C", "command": temizle, "pos": (225, 580)},
+    {"text": "⌫", "command": sil, "pos": (225, 580)},
+]
 
+# Create buttons
+for button in buttons:
+    width = button.get("width", 4)
+    ttk.Button(window, width=width, text=button["text"], command=button["command"]).place(height=44, x=button["pos"][0], y=button["pos"][1])
 
-
-Button(window, width=1, text="...", fg="black", font=("Helvetica", 9),  background='white',command=ikinci_pencere).place(height=15,x=1, y=1)
-# Yenileme butonu
-#Button(window, text="↻",background='white',font=("Times", 15),width=2, command=yenile).place(height=23, x=130, y=1)
-
-window.bind("<Return>", lambda event: birim_donustur())
+# Klavye bağlamaları
+window.bind("<Return>", lambda event: hesapla())
+window.bind("<KP_Divide>", lambda event: islemler("/"))
+window.bind("<KP_Multiply>", lambda event: islemler("*"))
+window.bind("<KP_Subtract>", lambda event: islemler("-"))
+window.bind("<KP_Add>", lambda event: islemler("+"))
 window.bind("<BackSpace>", lambda event: sil())
-window.bind("<KP_Enter>", lambda event: birim_donustur())
-window.bind("<Key>", lambda event: klavye_islemleri(event))
+window.bind("<KP_Enter>", lambda event: hesapla())
 
 def klavye_islemleri(event):
     if event.char in '0123456789':
         yaz(event.char)
+    elif event.char in '+-*/%':
+        islemler(event.char)
     elif event.char == '\r':
-        birim_donustur()
+        hesapla()
     elif event.char == '.':
         yaz('.')
 
 window.mainloop()
-
